f2a1d647bb3f373f6857d3346a79e10f04f5e2b5
Vlad Smirnov <vlad@colugo-sys.com>
2024-02-15 15:25:33 +0200

    Added GPS altitude value to bottom display panel.

--- src/Vehicle/VehicleGPSFactGroup.h
+++ src/Vehicle/VehicleGPSFactGroup.h
@@ -16,46 +16,77 @@ class VehicleGPSFactGroup : public FactGroup
 {
     Q_OBJECT
 
-public:
-    VehicleGPSFactGroup(QObject* parent = nullptr);
+  public:
+    VehicleGPSFactGroup(QObject *parent = nullptr);
 
-    Q_PROPERTY(Fact* lat                READ lat                CONSTANT)
-    Q_PROPERTY(Fact* lon                READ lon                CONSTANT)
-    Q_PROPERTY(Fact* mgrs               READ mgrs               CONSTANT)
-    Q_PROPERTY(Fact* hdop               READ hdop               CONSTANT)
-    Q_PROPERTY(Fact* vdop               READ vdop               CONSTANT)
-    Q_PROPERTY(Fact* courseOverGround   READ courseOverGround   CONSTANT)
-    Q_PROPERTY(Fact* count              READ count              CONSTANT)
-    Q_PROPERTY(Fact* lock               READ lock               CONSTANT)
+    Q_PROPERTY(Fact *lat READ lat CONSTANT)
+    Q_PROPERTY(Fact *lon READ lon CONSTANT)
+    Q_PROPERTY(Fact *alt READ alt CONSTANT)
+    Q_PROPERTY(Fact *mgrs READ mgrs CONSTANT)
+    Q_PROPERTY(Fact *hdop READ hdop CONSTANT)
+    Q_PROPERTY(Fact *vdop READ vdop CONSTANT)
+    Q_PROPERTY(Fact *courseOverGround READ courseOverGround CONSTANT)
+    Q_PROPERTY(Fact *count READ count CONSTANT)
+    Q_PROPERTY(Fact *lock READ lock CONSTANT)
 
-    Fact* lat               () { return &_latFact; }
-    Fact* lon               () { return &_lonFact; }
-    Fact* mgrs              () { return &_mgrsFact; }
-    Fact* hdop              () { return &_hdopFact; }
-    Fact* vdop              () { return &_vdopFact; }
-    Fact* courseOverGround  () { return &_courseOverGroundFact; }
-    Fact* count             () { return &_countFact; }
-    Fact* lock              () { return &_lockFact; }
+    Fact *lat()
+    {
+        return &_latFact;
+    }
+    Fact *lon()
+    {
+        return &_lonFact;
+    }
+    Fact *alt()
+    {
+        return &_altFact;
+    }
+    Fact *mgrs()
+    {
+        return &_mgrsFact;
+    }
+    Fact *hdop()
+    {
+        return &_hdopFact;
+    }
+    Fact *vdop()
+    {
+        return &_vdopFact;
+    }
+    Fact *courseOverGround()
+    {
+        return &_courseOverGroundFact;
+    }
+    Fact *count()
+    {
+        return &_countFact;
+    }
+    Fact *lock()
+    {
+        return &_lockFact;
+    }
 
     // Overrides from FactGroup
-    virtual void handleMessage(Vehicle* vehicle, mavlink_message_t& message) override;
+    virtual void handleMessage(Vehicle *vehicle, mavlink_message_t &message) override;
 
-    static const char* _latFactName;
-    static const char* _lonFactName;
-    static const char* _mgrsFactName;
-    static const char* _hdopFactName;
-    static const char* _vdopFactName;
-    static const char* _courseOverGroundFactName;
-    static const char* _countFactName;
-    static const char* _lockFactName;
+    static const char *_latFactName;
+    static const char *_lonFactName;
+    static const char *_altitudeFactName;
+    static const char *_mgrsFactName;
+    static const char *_hdopFactName;
+    static const char *_vdopFactName;
+    static const char *_courseOverGroundFactName;
+    static const char *_countFactName;
+    static const char *_lockFactName;
 
-protected:
-    void _handleGpsRawInt   (mavlink_message_t& message);
-    void _handleHighLatency (mavlink_message_t& message);
-    void _handleHighLatency2(mavlink_message_t& message);
+  protected:
+    void _handleGpsRawInt(mavlink_message_t &message);
+    void _handleHighLatency(mavlink_message_t &message);
+    void _handleHighLatency2(mavlink_message_t &message);
 
     Fact _latFact;
     Fact _lonFact;
+    Fact _altFact;
     Fact _mgrsFact;
     Fact _hdopFact;
     Fact _vdopFact;
